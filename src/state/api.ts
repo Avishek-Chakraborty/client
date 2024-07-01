4ce1746f5ed0aca735fc48232e5aa3b9a761991d
Avishek Chakraborty <avishek3141@gmail.com>
2024-07-01 23:30:07 +0530

    fix API call

--- src/state/api.ts
+++ src/state/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { GetKpisResponse, GetProductsResponse, GetTransactionsResponse } from "@/state/types";
 
 export const api = createApi({
-	baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
+	baseQuery: fetchBaseQuery({ baseUrl: 'https://financebackend.fr.to/' }),
 	reducerPath: "main",
 	tagTypes: ["Kpis", "Products", "Transactions"],
 	endpoints: (build) => ({
